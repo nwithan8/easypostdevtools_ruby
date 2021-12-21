4f9ff299916545a03ca9d16696f14e87c5788fdc
Nate Harris <nharris@easypost.com>
2021-12-20 22:14:44 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in easypostdevtools.gemspec
+gemspec
+
+gem "minitest", "~> 5.0"
+
+gem "rubocop", "~> 1.21"
---
+++ lib/easypostdevtools.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "easypost"
+
+require_relative "easypostdevtools/version"
+
+module EasyPostDevTools
+
+  class Error < StandardError; end
+  # Your code goes here...
+end
